b8f5ccb35f8bb04429c1a8932c95b2731a747f34
김지수 <73218962+jisoo03010@users.noreply.github.com>
2022-02-07 23:49:46 +0900

    Add files via upload

--- 포포를 찾아라!/common.js
+++ 포포를 찾아라!/common.js
@@ -1,6 +1,5 @@
+//첫 화면 부드럽게 화면 넘어가는 애니메이션 
 let container = document.getElementById("container")
-
-
 setInterval(function time() {
     container.style.opacity = 0
     container.style.zIndex = 0
@@ -9,8 +8,10 @@ setInterval(function time() {
 let contents = document.getElementById("contents")
 setInterval(function time1() {
     contents.style.opacity = 1
+
 }, 2700)
 
+// 게임 설명 팝업 
 let popup = document.getElementById("pop")
 let game_field = document.getElementById("game_field")
 popup.addEventListener("click", function btn() {
@@ -21,120 +22,96 @@ popup.addEventListener("click", function btn() {
     }
 })
 
-let play = document.getElementById("play")
-let i = document.getElementById("i")
-let newI = document.getElementById("newI")
-let text = document.getElementById("text")
-let randomImage = document.getElementById("randomImage")
+let play = document.getElementById("play") //play버튼 
+let i = document.getElementById("i") //플레이 버튼 왼쪽 아이콘 
+let text = document.getElementById("text") // play 텍스트 적힌 span 태그 
+
+let newI = document.getElementById("newI") //stop 아이콘 
+
+let randomImage = document.getElementById("randomImage") //랜덤한 이미지가 들어갈  div 태그 
+
 let randomNumber = Math.floor(Math.random() * 30) + 1;
 
-let afterReplayNumver = Math.floor(Math.random() * 30) + 1;
+//시간이 종료되거나 게임이 끝나면 나타나는 리플레이를 권유하는 팝업 창 
+var timeOut_AfterReplay = document.getElementById("timeOut_AfterReplay")
 
 
-let repalybutton = document.getElementById("repalybutton")
-let timeOut_AfterReplay = document.getElementById("timeOut_AfterReplay")
-
-
-
-//다시 게임 실행하는 버튼 이벤트 
-repalybutton.addEventListener("click", function () {
-
-    timeOut_AfterReplay.style.display = "none"
-    document.getElementById("time").innerHTML = "05" + ":00"
-    randomImage.style.backgroundImage = "url(https://kimmsoll.github.io/whale-game-js/imgs/icons/" + afterReplayNumver + ".png)"
-
-    let gamepage = document.getElementById("gamepage")
-    let child = document.createElement('div')
-    let divCreate = gamepage.appendChild(child)
-    divCreate.id = "imgsIconBox2"
-    let imgsIconBox2 = document.getElementById("imgsIconBox2")
-    for (let j = 1; j < 31; j++) {
-        const image2 = new Image();
-        image2.src = "https://kimmsoll.github.io/whale-game-js/imgs/icons/" + j + ".png";
-        imgsIconBox2.appendChild(image2);
-
-        image2.addEventListener("click", function () {
-            if ("https://kimmsoll.github.io/whale-game-js/imgs/icons/" + afterReplayNumver + ".png" == image2.src) {
-                console.log(randomNumber + "둘의 값이 같음 ");
-            } else {
-                console.log("다름 ");
-            }
-        })
-    }
-    //타이머 개발
-    var count2 = 5;
-    var counter2 = setInterval(timer, 1000);
-    function timer() {
-
-        count2--;
-        if (count2 <= 0) {
-
-            clearInterval(counter2);
-            document.getElementById("time").innerHTML = "00" + ":00"
-        }
-        document.getElementById("time").innerHTML = "0" + count2 + ":00";
-        //시간이 종료되었을때 발생하는 이벤트 
-        if (document.getElementById("time").innerText == '00:00') {
-            console.log('시간 종료 !! 게임 끝!!');
-            document.getElementById("timeOut_AfterReplay").style.display = "block"
-            imgsIconBox2.remove('img')
-        }
-    }
-})
-
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //play 클릭하면 발생하는 이벤트 
 play.addEventListener("click", function () {
+
+    let imgsIconBox = document.getElementById("imgsIconBox")
     //랜덤으로 이미지 
     randomImage.style.backgroundImage = "url(https://kimmsoll.github.io/whale-game-js/imgs/icons/" + randomNumber + ".png)"
 
+    /* 상단 play버튼의 아이콘을 none으로 하고 innertext를 stop으로 변경 및 왼쪽 아이콘 변경하는 부분  */
     i.style.display = "none"
-
     newI.style.display = "block"
     text.replaceWith("STOP")
 
-
     //타이머 개발
     var count = 5;
-    var counter = setInterval(timer, 1000);
-    function timer() {
+
+
+
+    // timer 함수를 1초 후에 실행 5에서 0으로 줄어들음 
+    var counter = setInterval(function timer1() {
 
         document.getElementById("time").innerHTML = "05" + ":00"
-        count--;
-        if (count <= 0) {
 
-            clearInterval(counter);
-            document.getElementById("time").innerHTML = "00" + ":00"
-        }
+        count--
         document.getElementById("time").innerHTML = "0" + count + ":00";
+
+
         //시간이 종료되었을때 발생하는 이벤트 
         if (document.getElementById("time").innerText == '00:00') {
             console.log('시간 종료 !! 게임 끝!!');
-            document.getElementById("timeOut_AfterReplay").style.display = "block"
+            timeOut_AfterReplay.style.display = "block"
             imgsIconBox.remove()
-
         }
-
-    }
-
-    let imgsIconBox = document.getElementById("imgsIconBox")
+        if (count <= 0) {
+            document.getElementById("time").innerHTML = "00" + ":00"
+            //counter 함수 삭제 
+            clearInterval(counter);
+        }
+    }, 1000);
+    //랜덤한 이미지가 나옴
     for (let i = 1; i < 31; i++) {
         const image = new Image();
         image.src = "https://kimmsoll.github.io/whale-game-js/imgs/icons/" + i + ".png";
         imgsIconBox.appendChild(image);
 
+        //이미지 클릭시 이벤트 
         image.addEventListener("click", function () {
+
+            //랜덤 숫자값과 사용자가 선택한 이미지의 url숫자가 같은지 틀린지 확인 
             if ("https://kimmsoll.github.io/whale-game-js/imgs/icons/" + randomNumber + ".png" == image.src) {
-                console.log(randomNumber + "둘의 값이 같음 ");
+                console.log(randomNumber + "random둘의 값이 같음 "); //console
+
+                document.getElementById("time").innerText = '00:00'
+
+                count = 0
+
+                timeOut_AfterReplay.style.display = "none"
+                document.getElementById("game_success").style.display = "block"
+                imgsIconBox.style.display = "none"
             } else {
                 console.log("다름 ");
             }
         })
     }
 
+
     game_field.style.display = "none"
 
 })
+
+
+/*========================================================= */
+
+//점수 판 
+
 
 let score = document.getElementById("score")
 let backgroundBanner = document.getElementById("backgroundBanner")
@@ -145,8 +122,3 @@ let closes = document.getElementById("close")
 closes.addEventListener("click", function () {
     backgroundBanner.style.display = "none"
 })
-
-
-//소리 추가
-var audio = new Audio('./sound/Bubbles1.wav')
-audio.play();
